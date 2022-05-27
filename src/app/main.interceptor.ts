224dd48156cbfce1022b221bd7f80a72bbfe071d
CalmDeveloper <demir445@gmail.com>
2022-05-27 21:13:21 +0300

    end access token

--- src/app/main.interceptor.ts
+++ src/app/main.interceptor.ts
@@ -5,33 +5,53 @@ import {
     HttpEvent,
     HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import {catchError, Observable, switchMap, throwError} from 'rxjs';
 import {AuthService} from "./services";
 import {Router} from "@angular/router";
+import {IToken} from "./interfaces";
 
 @Injectable()
 export class MainInterceptor implements HttpInterceptor {
-
+isRefreshing =false;
     constructor(private authService: AuthService, private router: Router) {
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         const isAutheticated = this.authService.isAuthorization();
         if (isAutheticated) {
-            request = this.addToken(request, this.authService.getToken())
+            request = this.addToken(request, this.authService.getAccessToken())
         }
         return next.handle(request).pipe(
             catchError((res: HttpErrorResponse) => {
                 if (res && res.error && res.status === 401) {
-                    this.authService.delateToken()
-                    this.router.navigate(['login']).then()
+                    return this.handle401Error(request, next)
+
                 }
-                return throwError(() => new Error('token invalid or expired!'))
+                return throwError(() => new Error('token invalid or expired'))
             })
-        )
+        ) as any;
     }
 
     addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
-        return request.clone({setHeaders: {Authorization: `Bearer ${token}`}})
+        return request.clone({
+            setHeaders: {Authorization: `Bearer ${token}`}
+        })
+    }
+
+    handle401Error(request: HttpRequest<any>, next: HttpHandler): any {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+            return this.authService.refresh().pipe(
+                switchMap((tokens: IToken) => {
+                    return next.handle(this.addToken(request, tokens.access))
+                }),
+                catchError(() => {
+                    this.isRefreshing = false
+                    this.authService.delateToken();
+                    this.router.navigate(['login'])
+                    return throwError(() => new Error('token invalid or expired'))
+                })
+            )
+        }
     }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IToken, IUser} from "../interfaces";
 import {Observable} from "rxjs";
-import {urls} from "../constants/urls";
+import {urls} from "../constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
  private accessTokenKey= 'access'
+    private refreshTokenKey= 'refresh'
   constructor(private httpClient: HttpClient) {
   }
 
@@ -19,15 +20,26 @@ export class AuthService {
     return this.httpClient.post<IToken>(urls.auth, user)
   }
   setToken(token:IToken):void{
-    return localStorage.setItem(this.accessTokenKey,token.access)
+     localStorage.setItem(this.accessTokenKey,token.access)
+      localStorage.setItem(this.refreshTokenKey,token.refresh)
   }
-  getToken():string{
-     return localStorage.getItem(this.accessTokenKey) as string
-  }
+
     isAuthorization():boolean{
         return !!localStorage.getItem(this.accessTokenKey)
     }
     delateToken():void{
      localStorage.removeItem(this.accessTokenKey)
+        localStorage.removeItem(this.refreshTokenKey)
+    }
+    refresh():Observable<IToken>{
+     const refresh = this.getRefreshToken()
+    return this.httpClient.post<IToken>(`${urls.auth}/ refersh`,{refresh})
+    }
+    getAccessToken():string{
+        return localStorage.getItem(this.accessTokenKey) as string
+    }
+    getRefreshToken():string{
+        return localStorage.getItem(this.refreshTokenKey) as string
     }
 }
+
